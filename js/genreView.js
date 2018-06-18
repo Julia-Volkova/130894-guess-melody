a9e456877b17fdadb2286d2272c01810a78ab63c
Julia <julia.volkova090@gmail.com>
2018-06-18 15:55:35 +0300

    Module5-task1 4

--- js/genreView.js
+++ js/genreView.js
@@ -73,7 +73,7 @@ export default class GenreView extends AbstractView {
     });
   }
 
-  onSwitch(evt) {
+  onSwitch() {
 
   }
 }
--- js/performerView.js
+++ js/performerView.js
@@ -48,7 +48,7 @@ export default class PerformerView extends AbstractView {
     });
   }
 
-  onSwitch(evt) {
+  onSwitch() {
 
   }
 }
